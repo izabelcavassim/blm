d032590b971bfd8f0f378023fb173737367eb8a8
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2016-12-26 19:50:44 +0100

    From plot to ggplot and added one more plot

---
+++ R/plot.R
@@ -0,0 +1,44 @@
+#' @title Plot blm object
+#'
+#' @description It returns different plots of object blm
+#' @parm object an blm object for which the plots are desired.
+#' @parm ... additional arguments.
+#' @return plot of Residuals vs Fitted values.
+#' @export
+#' @seealso blm, fitted.blm, residuals.blm
+
+plot.blm <- function(object, ...) {
+  data = object$data
+
+  if(is.null(data) == FALSE && ncol(data) == 2){
+
+    # First plot: fitted values vs residuals
+    fit = fitted(object)
+    resid = residuals(object)
+
+    # Transforming in a dataframe
+    plot_data = as.data.frame(cbind(fit, resid))
+
+    # Plotting with smoothing function defined by the formula of blm object
+    p1 <- ggplot(plot_data, aes(x = fit, y = resid)) + geom_point() + geom_smooth(formula = object$formula, colour = 'red') + theme_bw()
+    p1 <- p1 + geom_hline(yintercept=0, linetype="dotted") +
+    labs(
+      x = 'Fitted values',
+      y = 'Residuals',
+      title = 'Residuals vs Fitted'
+    )
+
+    return(p1)
+
+    # p2: Standardized residuals vs Theoretical quantiles
+
+    # Standardized residual:
+    sd_resid = resid/sd(resid)
+
+    # Theoretical Quantile plot:
+    qqplot(resid(test_blm), sd_resid, xlab = 'Theoretical Quantiles', ylab = 'Standardized residuals', main = 'Normal Q-Q plot')
+  }
+  if(is.null(data) == FALSE && ncol(data) > 2){
+    # I DONT KNOW YET
+  }
+}
