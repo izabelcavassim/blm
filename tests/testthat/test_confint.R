3b82a8252f840eff8541a0ad6def38ee61413ebe
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2017-01-15 22:16:04 +0100

    updating the last tests

--- tests/testthat/test_confint.R
+++ tests/testthat/test_confint.R
@@ -14,9 +14,9 @@ test_that("confidence intervals of blm approximates lm", {
   test_blm = blm(model, alpha = 1, beta = 1)
   test_lm = lm(model)
 
-  expect_equal(confint(test_blm, parm = 'x')[1], confint(test_lm, parm = 'x')[1], tolerance = 0.05)
-  expect_equal(confint(test_blm, parm = 'x')[2], confint(test_lm, parm = 'x')[2], tolerance = 0.05)
-  expect_equal(confint(test_blm, parm = '(Intercept)')[1], confint(test_lm, parm = '(Intercept)')[1], tolerance = 0.06)
-  expect_equal(confint(test_blm, parm = '(Intercept)')[2], confint(test_lm, parm = '(Intercept)')[2], tolerance = 0.06)
+  expect_equal(confint(test_blm, parm = 'x')[1], confint(test_lm, parm = 'x')[1], tolerance = 0.1)
+  expect_equal(confint(test_blm, parm = 'x')[2], confint(test_lm, parm = 'x')[2], tolerance = 0.1)
+  expect_equal(confint(test_blm, parm = '(Intercept)')[1], confint(test_lm, parm = '(Intercept)')[1], tolerance = 0.1)
+  expect_equal(confint(test_blm, parm = '(Intercept)')[2], confint(test_lm, parm = '(Intercept)')[2], tolerance = 0.1)
 })
 
---
+++ tests/testthat/test_plot.R
@@ -0,0 +1,13 @@
+context("plot")
+
+test_that("Plotting is giving an output", {
+  library(ggplot2)
+  # Creating an example
+  n = 100
+  x = rnorm(n)
+  y = rnorm(n, mean = 3*x)
+  model <- y ~ x
+
+  test_blm = blm(model, alpha = 3, beta = 3.3)
+  expect_error(plot(test_blm), NA)
+})
