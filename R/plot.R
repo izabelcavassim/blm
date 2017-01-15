49638add1d59ce0f40cb694b990b546137e1e4d1
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2017-01-15 12:46:12 +0100

    Plot function now outputs y x relationship

--- R/plot.R
+++ R/plot.R
@@ -9,46 +9,50 @@
 plot.blm <- function(object, ...) {
   data = object$data
 
-  if(is.null(data) == FALSE && ncol(data) == 2){
+  # First plot: fitted values vs residuals
+  fit = fitted(object)
+  resid = residuals(object)
 
-    # First plot: fitted values vs residuals
-    fit = fitted(object)
-    resid = residuals(object)
+  # Transforming in a dataframe
+  plot_data = as.data.frame(cbind(fit, resid))
 
-    # Transforming in a dataframe
-    plot_data = as.data.frame(cbind(fit, resid))
-
-    # Plotting with smoothing function defined by the formula of blm object
-    p1 =  ggplot2::ggplot(plot_data, ggplot2::aes(x = fit, y = resid)) +  ggplot2::geom_point() +  ggplot2::geom_smooth(formula = object$formula, colour = 'red') +  ggplot2::theme_bw()
-    p1 = p1 +  ggplot2::geom_hline(yintercept=0, linetype="dotted") +
-    ggplot2::labs(
+  # Plotting with smoothing function defined by the formula of blm object
+  p1 =  ggplot2::ggplot(plot_data, ggplot2::aes(x = fit, y = resid)) +  ggplot2::geom_point() +  ggplot2::geom_smooth(formula = object$formula, colour = 'red') +  ggplot2::theme_bw()
+  p1 = p1 +  ggplot2::geom_hline(yintercept=0, linetype="dotted") +
+  ggplot2::labs(
       x = 'Fitted values',
       y = 'Residuals',
       title = 'Residuals vs Fitted'
-    )
+  )
 
 
-    # Second plot: Standardized residuals vs Theoretical quantiles
+  # Second plot: Standardized residuals vs Theoretical quantiles
 
-    # Standardized residual:
-    sd_resid = resid/sd(resid)
+  # Standardized residual:
+  sd_resid = resid/sd(resid)
 
-    # Theoretical Quantile plot:
-    plot2_data = as.data.frame(cbind(resid(object), sd_resid))
-    p2 = ggplot2::ggplot(plot2_data, ggplot2::aes(sample = sd_resid)) +  ggplot2::theme_bw()
-    p2 = p2 + ggplot2::stat_qq() +
-    ggplot2::labs(
-        x = 'Theoretical Quantiles',
-        y = 'Standardized residuals',
-        title = 'Normal Q-Q plot'
-    )
-    # Third plot: Fitted values vs square root of standardized residuals
-    plot3_data = as.data.frame(cbind(fit, sd_resid))
-    p3 = ggplot2::qplot(plot3_data[,1], plot3_data[,2], main = 'Scale-Location', xlab = 'Fitted values', ylab='sqrt(Standardized residuals)') + ggplot2::theme_bw()
+  # Theoretical Quantile plot:
+  plot2_data = as.data.frame(cbind(resid(object), sd_resid))
 
+  y <- quantile(plot2_data$sd_resid[!is.na(plot2_data$sd_resid)], c(0.25, 0.75))
+  x <- qnorm(c(0.25, 0.75))
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
 
-    gridExtra::grid.arrange(p1, p2,p3, ncol=2, nrow =2)
+  p2 = ggplot2::ggplot(plot2_data, ggplot2::aes(sample = sd_resid)) +  ggplot2::theme_bw()
+  p2 = p2 + ggplot2::stat_qq()
+  p2 = p2 +  ggplot2::geom_abline(intercept=int, linetype="dotted", slope = slope) +
+  ggplot2::labs(
+      x = 'Theoretical Quantiles',
+      y = 'Standardized residuals',
+      title = 'Normal Q-Q plot'
+  )
 
-  }
+  # Third plot: Y x X
+  plot3_data = as.data.frame(object$data)
+  p3 = ggplot2::qplot(plot3_data[,2], plot3_data[,1], main = 'Relationship', xlab = 'Predictor (x)', ylab='Response (y)') + ggplot2::theme_bw() +  ggplot2::geom_smooth(formula = object$formula, colour = 'red')
+
+  gridExtra::grid.arrange(p1, p2,p3, ncol=2, nrow =2)
+
 
   }
