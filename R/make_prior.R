b5eabd3b105e189e21347a36e033a8c761c92e69
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2017-01-15 12:46:58 +0100

    polishing documentation

--- R/make_prior.R
+++ R/make_prior.R
@@ -1,12 +1,12 @@
 
 #' @title Make the Prior Distribution
 #'
-#' @description It takes the model description and the prior precision alpha to build the prior distribution
+#' @description It takes the model description and the prior precision alpha to build the prior distribution.
 #'
 #' @param model A formula describing the model.
-#' @param alpha The prior precision, the hyperparameter of Prior Distribution
-#' @param ... additional data
-#' @return The mean and covariance matrix
+#' @param alpha The prior precision, the hyperparameter of Prior Distribution.
+#' @param ... additional data.
+#' @return The mean and covariance matrix.
 #' @export
 
 make_prior <- function(model, alpha, ...){
